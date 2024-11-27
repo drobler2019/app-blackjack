35a3ff748144b7f45c68c2828a49ed33f3e5d580
drobler2019 <drobler2019@gmail.com>
2024-11-26 19:17:28 -0500

    Revert "first commit"
    This reverts commit 447e3077267093ba6c5426626db04c2ea38fae69.

--- app/servicios/CrearNuevaCarta.js
+++
@@ -1,7 +0,0 @@
-export const nuevaCarta = (carta) => {
-    const nuevaCarta = document.createElement('img');
-    nuevaCarta.src = `../public/assets/static/${carta}.png`;
-    nuevaCarta.alt = "carta";
-    nuevaCarta.width = "90";
-    return nuevaCarta;
-};
